219cbd7faae12eed68cf6ad5cd71a025f3bbc13e
dulee <duckyounglee@oden.kr>
2022-06-15 15:20:35 +0900

    need data to check success

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { RolesGuard } from './roles/roles.guard';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Role } from './roles/role.enum';
+import { Roles } from './roles/roles.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @Roles(Role.Admin)
+  @UseGuards(RolesGuard)
   getHello(): string {
     return this.appService.getHello();
   }
---
+++ src/roles/role.enum.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  User = 'user',
+  Staff = 'staff',
+  Admin = 'admin',
+}
